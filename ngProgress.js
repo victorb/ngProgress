32c1684f2798e1a22f0bbafa7ee4b3376006d213
victorbjelkholm <victorbjelkholm@gmail.com>
2013-08-21 06:04:31 +0200

    Crossbrowser fixing, option to set provider config in bootstrap

--- ngProgress.js
+++ ngProgress.js
@@ -2,28 +2,36 @@ var module = angular.module('ngProgress', []);
 
 module.provider('progressbar', function() {
     this.count = 0;
+    this.height = '2px';
+    this.color = 'firebrick';
      
     this.$get = function($document, $window) {
         var count = this.count;
+        var height = this.height;
+        var color = this.color;
         var $body = $document.find('body');
         var progressbarContainer = angular.element('<div class="progressbar-container"></div>');
         var progressbar = angular.element('<div class="progressbar"></div>');
-        var stylesheet = document.styleSheets[document.styleSheets.length - 1];
+        
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".progressbar {-webkit-transition: all 0.5s ease-in-out; -moz-transition: all 0.5s ease-in-out; -o-transition: all 0.5s ease-in-out; transition: all 0.5s ease-in-out;}";
+        document.body.appendChild(css);
 
         progressbarContainer.css('position', 'fixed');
         progressbarContainer.css('top', '0');
         progressbarContainer.css('left', '0');
         progressbarContainer.css('right', '0');
 
-        progressbar.css('height', '2px');
-        progressbar.css('box-shadow', '0px 0px 10px 0px firebrick');
+        progressbar.css('height', height);
+        progressbar.css('box-shadow', '0px 0px 10px 0px ' + color);
         progressbar.css('width', count+'%');
-        progressbar.css('background-color', 'firebrick');
+        progressbar.css('background-color', color);
 
-        stylesheet.insertRule(".progressbar {-webkit-transition: all 0.5s ease-in-out; }", 0);
-        stylesheet.insertRule(".progressbar {-moz-transition: all 0.5s ease-in-out; }", 0);
-        stylesheet.insertRule(".progressbar {-o-transition: all 0.5s ease-in-out; }", 0);
-        stylesheet.insertRule(".progressbar {transition: all 0.5s ease-in-out; }", 0);
+        // stylesheet.insertRule(".progressbar {-webkit-transition: all 0.5s ease-in-out; }", 0);
+        // stylesheet.insertRule(".progressbar {-moz-transition: all 0.5s ease-in-out; }", 0);
+        // stylesheet.insertRule(".progressbar {-o-transition: all 0.5s ease-in-out; }", 0);
+        // stylesheet.insertRule(".progressbar {transition: all 0.5s ease-in-out; }", 0);
 
         progressbarContainer.append(progressbar);
         $body.append(progressbarContainer);
@@ -75,7 +83,9 @@ module.provider('progressbar', function() {
             complete: function() {
                 count = 100;
                 progressbar.css('width', count + '%');
-                progressbar.css('opacity', '0');
+                setTimeout(function(){
+                    progressbar.css('opacity', '0');
+                }, 500);
                 setTimeout(function(){
                     count = 0;
                     progressbar.css('width', count + '%');
@@ -84,4 +94,13 @@ module.provider('progressbar', function() {
             }
         }
     };
+
+    this.setColor = function(color) {
+        this.color = color;
+    };
+
+    this.setHeight = function(height) {
+        this.height = height;
+    };
+
 });
