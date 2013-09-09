6032109e56667af4844bc0e32705fcc75753fd7a
victorbjelkholm <victorbjelkholm@gmail.com>
2013-09-09 13:02:07 +0200

    Growl shouldn't be in karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
 
     // test results reporter to use
     // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
-    reporters: ['progress', 'growl'],
+    reporters: ['progress'],
 
 
     // web server port
